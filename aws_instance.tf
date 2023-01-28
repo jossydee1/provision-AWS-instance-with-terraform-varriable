10a4b1b17c3e87a2640b73befa45f6d1ae152bfb
jossydee1 <jossydee1@gmail.com>
2023-01-28 04:50:18 +0100

    Revert "terraform config files"
    This reverts commit 5881effd8108cb3a34a2c845cc3d923bea74aa5c.

--- aws_instance.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "terraform" {
-  ami           = "${lookup(var.ami_id, var.region)}"
-  instance_type = "t2.micro"
-}
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-access_key = "AKIARDJQWWXGW2GFVOMS"
-secret_key = "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"
